9a321539c01db3b2831e41ed31a2c59f6d208c16
L-DESKTOP\Lawrence <volodars@sheridancollege.ca>
2025-07-28 18:00:37 -0400

    Started Assinment 2

---
+++ Assets/Assignment 2/Spin Me Round.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinMeRound : MonoBehaviour
+//{
+//    //blades rotate around center on spacebar
+//    public float speed;
+//    bool on;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        on = false;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//        bool Spacebar = Input.GetKeyDown(KeyCode.Space);
+//        //code that determines if the fan is being turned on/off
+//        if (on == true && Spacebar == true)
+//        {
+//            on = false;
+//            speed = 0f;
+//        }
+//        else if (on == false && Spacebar == true)
+//        {
+//            on = true;
+//            speed = 0.5f;
+//        }
+
+//        //code that lets the fan blades rotate around the fan center
+//        Vector3 newRotation = transform.eulerAngles;
+//        newRotation.z += speed;
+//        transform.eulerAngles = newRotation;
+//    }
+//}
---
+++ Assets/Assignment 2/Zeusian.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zeusian : MonoBehaviour
+//{
+//    //Jupiter shrinks on button press
+//    bool shrink; //a variable to determine whether or not Jupiter is visible
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        shrink = false;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        Vector3 newScale = transform.localScale;
+
+//        if (shrink == true)
+//        {
+//            newScale.x = 3.5f;
+//            newScale.y = 3.5f;
+//            transform.localScale = newScale;
+//        }
+//        else if (shrink == false)
+//        {
+//            newScale.x = 6.488745f;
+//            newScale.y = 6.488745f;
+//            transform.localScale = newScale;
+
+//        }
+//    }
+    
+//    public void OnClickShrink()
+//    {
+//        //if statements to shrink when button pressed
+//        if (shrink == true )
+//        {
+//            shrink = false;
+//        }
+//        else if (shrink == false)
+//        {
+//            shrink = true;
+//        }
+//    }
+}
